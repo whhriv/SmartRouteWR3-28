1b9651e919abb5cf01149cfd14a4e7f1efa4e4ae
William Reeder <whhriv@gmail.com>
2024-03-28 15:25:27 -0700

    nav bar collapse inline

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,32 +6,22 @@ import { Image } from 'react-bootstrap';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from 'react-router-dom';
 
-import '../navbar.css'
+import './navbar.css'
 function NavBar() {
   return (
-    <Navbar expand="sm" className="bg-body-tertiary" data-bs-theme="dark">
-      <Container >
-        {/* <Navbar.Brand className=" pt-2 pink " href="/">
-          SmartRoute
-        <Image src="../../public/smart_route_small_logo.png" alt="SmartRoute" style={{ height: '30px' }} />
-        </Navbar.Brand> */}
+    <Navbar expand="sm" className="bg-body-tertiary mx-auto flex-column flex-sm-row" data-bs-theme="dark">
+      <Container>
+    
        
-          <Nav className="mx-auto">
+          <Nav className="mx-auto flex-column flex-sm-row">
 
-          {/* <NavLink exact to="/createroute" activeClassName="activeClicked">CreateRoute</NavLink> */}
-
+      
             <Nav.Link className=" me-5" href="/CreateRoute">CREATE</Nav.Link>
-            <Nav.Link className="ms-5 me-5" href="/MapSpaceOld">VIEW</Nav.Link>
-            <Nav.Link className="ms-5 " href="/DirectionsList">LIST</Nav.Link>
-            {/* <Nav.Link href="/TabInputButton">Input Tests</Nav.Link>
-            <Nav.Link href="/LocationSearch">Location Search</Nav.Link>
-            <Nav.Link href="/ParentComponent">parent</Nav.Link>
-            <Nav.Link href="/APIComponent">API</Nav.Link>
-            <Nav.Link href="/GetDirectionsMapOver">GDMO</Nav.Link>
-            <Nav.Link href="/addremovestopoutside">ARSO</Nav.Link> */}
-
+            <Nav.Link className=" me-5" href="/MapSpaceOld">VIEW</Nav.Link>
+            <Nav.Link className="" href="/DirectionsList">LIST</Nav.Link>
+            
           </Nav>
-          <Navbar.Brand className=" pt-2 pink " href="/">
+          <Navbar.Brand className=" pink " href="/">
           
         <Image src="../../public/smart_route_small_logo.png" alt="SmartRoute" style={{ height: '40px' }} />
         </Navbar.Brand>
